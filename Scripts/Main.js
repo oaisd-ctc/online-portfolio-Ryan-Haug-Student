1ec8e449e323e99af4db3916c42287cf077bff59
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-05-19 13:09:26 -0400

    made a bottom row

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -8,18 +8,21 @@ function changeSlides(n) {
 
 function showSlides(n) { 
     let slides = document.getElementsByClassName("CertSlide");
+    let slidesText = document.getElementsByClassName("CertSlideTxt");
 
     //reset all slides to invisible
     for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slidesText[i].style.display = "none";
     }
 
-    //ensure bounds of img array
+    //ensure bounds of array
     if (n > slides.length)
-         {slideIndex = 1}
+        {slideIndex = 1}
     if (n < 1) 
         {slideIndex = slides.length}
 
-    //display current slide
+    //display current slide and text
     slides[slideIndex-1].style.display = "block";
+    slidesText[slideIndex-1].style.display = "block";
 }
