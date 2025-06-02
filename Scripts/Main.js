55344031b11ab9d99bfc091d8114a2650737461d
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-06-02 09:44:52 -0400

    temp2

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         particle.className = "js-particle";
         particle.style.left = Math.random() * 100 + "vw";
         particle.style.width = particle.style.height = Math.random() * 8 + 4 + "px";
-        particle.style.top = pageHeight + "px";
+        particle.style.top = pageHeight - 10 + "px";
 
         particle.style.animationDuration = 5 + Math.random() * 10 + "s";
         particle.style.animationDelay = Math.random() * 5 + "s";
