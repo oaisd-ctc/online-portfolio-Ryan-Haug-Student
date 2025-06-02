d6b1973f7faf4a57cc98b35e6f1964ee43348ae0
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-06-02 09:44:45 -0400

    temp

---
+++ Scripts/Main.js
@@ -0,0 +1,17 @@
+const particleCont = document.getElementById("ParticleContainer");
+const pageHeight = document.body.scrollHeight;
+
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < 50; i++) {
+        const particle = document.createElement("div");
+        particle.className = "js-particle";
+        particle.style.left = Math.random() * 100 + "vw";
+        particle.style.width = particle.style.height = Math.random() * 8 + 4 + "px";
+        particle.style.top = pageHeight + "px";
+
+        particle.style.animationDuration = 5 + Math.random() * 10 + "s";
+        particle.style.animationDelay = Math.random() * 5 + "s";
+
+        particleCont.appendChild(particle);
+    }
+}); 
